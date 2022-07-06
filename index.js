472b76dc5c69b2bba114b820ee8633cfa7440d28
Mauricio Erazo <antonierazo13@hotmail.com>
2022-07-06 17:36:38 +0200

    Compliting the task
    1. creating "public" folder and moving "documentation.html" in it
    
    2 creating bodyParser variable and installing and installing method-override
    
    3. setting up the logger with morgan mildware library
    
    4. adding the route for "documentation.html" with express.static
    
    5. adding the error-handling mildware
    
    6. creating a GET route for my top movies
    
    7. creating a GET route to show the "documentation.html" file

--- index.js
+++ index.js
@@ -3,16 +3,85 @@ const express = require('express'),
     fs = require('fs'), // import built in node modules fs and path
     path = require('path');
 
+const bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
+
 const app = express();
+
 // create a write stream (in append mode)
 // a ‘log.txt’ file is created in root directory
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' })
 
-// setup the logger
-app.use(morgan('combined', {stream: accessLogStream}));
+
+app.use(morgan('combined', { stream: accessLogStream })); // setup the logger, Mildware function
+
+app.use(express.static('public')); // Automatically routes all requests for static files to their corresponding files within a certain folder on the server
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json()); // 
+app.use(methodOverride());
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke! Sorry...')
+})
+
+let topMovies = [
+    {
+        title: 'The Shawshank Redemption',
+        author: 'Stephen King'
+    },
+    {
+        title: 'Lord of the Rings',
+        author: 'J.R.R. Tolkien'
+    },
+    {
+        title: 'Coco',
+        author: 'Lee Unkrich'
+    },
+    {
+        title: 'Braveheart',
+        author: 'Randall Wallace'
+    },
+    {
+        title: 'Midway',
+        author: 'Craig L. Symonds'
+    },
+    {
+        title: 'Red Tails',
+        author: 'John Ridley'
+    },
+    {
+        title: 'Batman The Dark knight',
+        author: 'Christopher Nolan'
+    },
+    {
+        title: 'Matrix',
+        author: 'Lana Wachowski'
+    },
+    {
+        title: 'John Wick',
+        author: 'Derek Kolstad'
+    },
+    {
+        title: 'Forrest Gump',
+        author: 'Winston Groom'
+    },
+];
 
 app.get('/', (req, res) => {
     res.send('Welcome to my app!');
+});
+
+app.get('/documentation', (req, res) => {
+    res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies)
 });
 
 app.get('/secreturl', (req, res) => {
