ad5479dbb1375f17fbe4ed6f29f9ab092581ef8c
Mauricio Erazo <antonierazo13@hotmail.com>
2022-07-06 15:52:34 +0200

    Appending Morgan logs to a file

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express'),
-    morgan = require('morgan')
+    morgan = require('morgan'),
+    fs = require('fs'), // import built in node modules fs and path
+    path = require('path');
 
 const app = express();
+// create a write stream (in append mode)
+// a ‘log.txt’ file is created in root directory
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' })
 
-app.use(morgan('common'));
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my app!');
+    res.send('Welcome to my app!');
 });
 
 app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
+    res.send('This is a secret url with super top-secret content.');
 });
 
 app.get('*', (req, res) => {
@@ -18,5 +24,5 @@ app.get('*', (req, res) => {
 })
 
 app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+    console.log('Your app is listening on port 8080.');
 });
