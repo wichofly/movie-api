4121beb28ace6aa7107f68536b36ed67fdea8b25
Mauricio Erazo <antonierazo13@hotmail.com>
2022-07-15 09:13:31 +0200

    Creating user schema

--- model.js
+++ model.js
@@ -20,3 +20,18 @@ let movieSchema = mongoose.Schema({
     imageURL: String,
     featured: Boolean
 });
+
+let userSchema = mongoose.Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    birthday: Date,
+    favoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}] 
+})
+
+// Making a model with the above schema, Mongoose is gonna create a collection with the name Movies in plural although that is written in singular.
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('user', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
