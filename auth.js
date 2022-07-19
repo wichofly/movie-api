9f866a6554ad6246a83d441d7a20aac4dcf0c511
Mauricio Erazo <antonierazo13@hotmail.com>
2022-07-19 11:35:52 +0200

    creating auth.js file
    importing auth  and passport file to index.js

---
+++ auth.js
@@ -0,0 +1,35 @@
+const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
+
+const jwt = require('jsonwebtoken'),
+    passport = require('passport');
+
+require('./passport.js');
+
+let generateJWTToken = (user) => {
+    return jwt.sign(user, jwtSecret, {
+        subject: user.username, // This is the username i am encoding in the JWT
+        expiresIn: '7d',
+        algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
+    });
+}
+
+// POST login
+module.exports = (router) => {
+    router.post('./login', (req, res) => {
+        passport.authenticate('local', { session: false }, (error, user, info) => {
+            if (error || !user) {
+                return res.status(400).json({
+                    message: 'Something went wrong',
+                    user: user
+                });
+            }
+            req.login(user, { session: false }, (error) => {
+                if (error) {
+                    res.send(error);
+                }
+                let token = generateJWTToken(user.toJson());
+                return res.json({ user, token });
+            });
+        })(req, res);
+    });
+}
