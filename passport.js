4b5a9321c369c8151c55077b7af8043bb777473a
Mauricio Erazo <antonierazo13@hotmail.com>
2022-07-19 11:03:32 +0200

    Create passport.js

---
+++ passport.js
@@ -0,0 +1,44 @@
+const passport = require('passport'),
+    localStrategy = require('passport-local').Strategy,
+    Models = require('./model.js'),
+    passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJwt;
+
+// Defines baisc HTTP authentication for login request
+passport.use(new localStrategy({
+    usernameField: 'username',
+    passwordField: 'password'
+}, (username, password, callback) => {
+    console.log(username + ' ' + password);
+    Users.findOne({ username: username }, (error, user) => {
+        if (error) {
+            console.log(error);
+            return callback(error);
+        }
+
+        if (!user) {
+            console.log('Incorrect username');
+            return callback(null, false, { message: 'Incorrect username or password' });
+        }
+
+        console.log('Finished');
+        return callback(null, user);
+    });
+}));
+
+// Allows to authenticate users based on the JWT submitted alongside their request.
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'your_jwt_secret'
+}, (jwtPayload, callback) => {
+    return Users.findById(jwtPayload._id)
+        .then((user) => {
+            return callback(null, user);
+        })
+        .catch((error) => {
+            return callback(error)
+        });
+}));
