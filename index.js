69b9994d90b6904aca6d8eeafafd0876384d1b71
Mauricio Erazo <antonierazo13@hotmail.com>
2022-08-01 12:52:32 +0200

    update app.use CORS
    trying to see the movies using parcel url 1234/movies

--- index.js
+++ index.js
@@ -38,7 +38,18 @@ app.use(express.static('public')); // Automatically routes all requests for stat
 app.use(bodyParser.json()); // support parsing of application/json type post data
 app.use(bodyParser.urlencoded({ extended: true })); //support parsing of application/x-www-form-urlencoded post data
 
-app.use(cors());
+let allowedOrigins = ['http://localhost:1234', 'http://testsite.com'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+        let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+        return callback(new Error(message ), false);
+      }
+      return callback(null, true);
+    }
+  }));
 
 let auth = require('./auth')(app); // note the app argument you're passing here. This ensures that Express is available in your “auth.js” file as well.
 
