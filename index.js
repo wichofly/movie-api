54fbe1f6d0015741d972f873aa9792e8d123e174
Mauricio Erazo <antonierazo13@hotmail.com>
2022-08-02 09:53:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -40,21 +40,21 @@ app.use(bodyParser.json()); // support parsing of application/json type post dat
 app.use(bodyParser.urlencoded({ extended: true })); //support parsing of application/x-www-form-urlencoded post data
 
 // Allows requests from all origins
-app.use(cors());
+//app.use(cors());
 
-//      // Allow certain origins to have access, replace app.use(cors()) and use:
-// let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
+     // Allow certain origins to have access, replace app.use(cors()) and use:
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
 
-// app.use(cors({
-//     origin: (origin, callback) => {
-//         if (!origin) return callback(null, true);
-//         if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-//             let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//             return callback(new Error(message), false);
-//         }
-//         return callback(null, true);
-//     }
-// }));
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 let auth = require('./auth')(app); // note the app argument you're passing here. This ensures that Express is available in your “auth.js” file as well.
 
