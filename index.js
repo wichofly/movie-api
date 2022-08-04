2b2f4f1f926c585a2e118f180cef96040639409e
Mauricio Erazo <antonierazo13@hotmail.com>
2022-08-04 15:27:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -438,6 +438,18 @@ app.get('/movies', function (req, res) { //passport.authenticate('jwt', { sessio
         });
 });
 
+// app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+//   Movies.find()
+//     .then((movies) => {
+//       res.status(201).json(movies);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send('Error: ' + err);
+//     })
+// });
+
+
 // READ
 /* 
 app.get('/movies/:title', (req, res) => {
